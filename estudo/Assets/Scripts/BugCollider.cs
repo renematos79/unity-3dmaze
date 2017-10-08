e1ddf7bf518bb1ba34c0faa2426c1dccf90afae9
renato matos <renato.matos79@gmail.com>
2017-10-08 20:20:42 -0300

    commit inicial

---
+++ estudo/Assets/Scripts/BugCollider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugCollider : MonoBehaviour {
+
+	public AudioClip CatchSound;
+	private AudioSource AudioSrc;
+
+	// Use this for initialization
+	void Start () {
+		AudioSrc = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			AudioSrc.Stop ();
+			AudioSrc.clip = CatchSound;
+			AudioSrc.Play ();
+			Destroy(this.gameObject);
+		}
+
+	}
+}
