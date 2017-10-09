4bb8b0bb5914b1ae694b29b4ad0911c257172e49
renato matos <renato.matos79@gmail.com>
2017-10-09 00:17:19 -0300

    versao inicial 1.0 para elaboracao do game e manual

---
+++ estudo/Assets/Scripts/GameEngine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEngine : MonoBehaviour {
+
+	public UnityEngine.UI.Text Label;
+	public int MaxEggs = 0;
+	private int Eggs;
+
+	// Use this for initialization
+	void Start () {
+		this.Eggs = 0;
+		if (this.Label != null) {
+			this.Label.text = this.MaxEggs.ToString ();
+		}		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void UpdateEggs(){
+		this.Eggs++;
+		if (this.Label != null) {
+			this.Label.text = (this.MaxEggs - this.Eggs).ToString();
+		}
+	}
+}
---
+++ estudo/Assets/Scripts/ItemCollider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollider : MonoBehaviour {
+
+	public AudioClip PlaySound;
+	public AudioSource AudioSrc;
+	public GameEngine Engine;
+
+	private CreateParticle ParticleInstance = null;
+
+	// Use this for initialization
+	void Start () {
+		this.ParticleInstance = GetComponent<CreateParticle> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			AudioManager.Instance.PlayClip(PlaySound);
+			if (this.ParticleInstance != null) {
+				this.ParticleInstance.DestroyParticle ();
+			}
+			if (this.Engine != null) {
+				this.Engine.UpdateEggs ();
+			}
+			Destroy(this.gameObject);
+		}
+	}
+}
