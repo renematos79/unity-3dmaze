b78f94453263eb9d7613f998461f0adb29295879
renato matos <renato.matos79@gmail.com>
2017-10-09 00:39:16 -0300

    separacao dos pacotes

---
+++ packages/scripts/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioSource AudioSrc;
+	public static AudioManager Instance = null;
+
+	// Use this for initialization
+	void Start () {
+		if (Instance == null) {
+			Instance = this;
+		} else if (Instance != this) {
+			Destroy (this.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayClip(AudioClip clip)
+	{
+		if (clip != null) {
+			AudioSrc.clip = clip;
+			AudioSrc.Play ();	
+		}
+	}
+
+	public void Stop(){
+		AudioSrc.Stop ();
+	}
+}
---
+++ packages/scripts/CreateParticle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateParticle : MonoBehaviour {
+
+	public GameObject Particle;
+	private GameObject ParticleInstance;
+
+	// Use this for initialization
+	void Start () {
+		if (this.Particle != null) {
+			ParticleInstance = Instantiate (this.Particle, this.gameObject.transform.position, Quaternion.identity);
+		} else {
+			ParticleInstance = null;
+		}
+	}
+
+	public void DestroyParticle(){
+		if (this.ParticleInstance != null) {
+			DestroyObject (this.ParticleInstance.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
